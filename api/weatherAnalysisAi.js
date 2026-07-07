bd377ed7d0effdb52f7972ba3cf2254dc168594c
SwapnilHadage <221566039+SwapnilHadage@users.noreply.github.com>
2026-07-07 11:06:08 +0530

    made debugging changes

--- api/weatherAnalysisAi.js
+++ api/weatherAnalysisAi.js
@@ -42,7 +42,7 @@ export default async function handler(req, res){
     console.error("Gemini error:", error);
 
     return res.status(500).json({
-      error: "AI request failed",
+      error: error,
     });
   }
   
