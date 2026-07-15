88c06c1d34d2d0e8476040d73659dd906e48923d
SwapnilHadage <221566039+SwapnilHadage@users.noreply.github.com>
2026-07-15 15:53:06 +0530

    debugging

--- src/components/AiAnalysis.jsx
+++ src/components/AiAnalysis.jsx
@@ -7,18 +7,42 @@ import './ai.css';
 function AiAnalysis() {
   const [res, setRes] = useState(null);
   let debugFlag = useRef(false);
+
   const getAnalysis = async(data)=>{
+    let analysisData = null;
     try {
-      const analysisData = await getWeatherAnalysis(data);
+      analysisData = await getWeatherAnalysis(data);
       console.log(analysisData);
     } catch (error) {
       console.error("Weather Analysis Failed", error);
     }
-    debugFlag.current = true;
+  debugFlag.current = true;
+
+  if(analysisData.answer){
+    try{
+      let jsonString = analysisData;
+      if(jsonString.includes('```json')){
+        jsonString = jsonString.replace(/```json\n?/g,'').replace(/```\n?/g,'')
+      }
+      const parsed = JSON.parse(jsonString);
+      if(parsed.answer && typeof parsed?.answer==='object'){
+          return parsed.answer;
+      }
+      return JSON.stringify(parsed, null, 2);
+    }catch(e){
+      setAiError(e.message);
+      return analysisData.answer;
+    }
   }
+
+  return res?.text ||
+    res?.message ||
+    (typeof res === "string" ? res : JSON.stringify(res, null, 2));
+  
+  }
+
+
   const { loading, error, currentWeatherData, todaysWeatherData, weekWeatherData, coords, language, } = useSelector(state=>state.setup);
-
-
   useEffect( ()=>{
     let data = null;
     if( !debugFlag.current &&
@@ -44,20 +68,13 @@ function AiAnalysis() {
       currentWeatherData,
       todaysWeatherData,
       weekWeatherData,
-    ])
-  const displayText =
-    res?.text ||
-    res?.message ||
-    (typeof res === "string" ? res : JSON.stringify(res, null, 2));
+    ]);
 
   return (
-    <div
-    className=" container
-    ">
+    <div className="container">
       <div className="box">
-        <div className="content"
-        >
-            <h1>{displayText}</h1>
+        <div className="content">
+            <h1>{res}</h1>
         </div>
       </div>
     </div>
